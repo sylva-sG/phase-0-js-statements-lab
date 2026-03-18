04635ff017e2d49c916ad9b787262b0a88a1a07b
sylva-sG <sylvansaoko@gmail.com>
2026-03-18 08:51:19 +0300

    Add if-else checks for temperature and timeOfDay, and a while loop for soilMoisture increment

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,23 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+// Check temperature
+if (temperature > 80) {
+  console.log("Watering on");
+} else {
+  console.log("Watering off");
+}
+
+// Check the time
+if (timeOfDay === "evening" || timeOfDay === "night") {
+  console.log("Lights on");
+} else {
+  console.log("Lights off");
+}
+
+// Increment until it reaches 40
+while (soilMoisture < 40) {
+  soilMoisture += 5;
+  console.log(soilMoisture);
+}
